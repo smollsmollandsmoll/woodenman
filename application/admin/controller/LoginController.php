8e627c399cbbbe03b6f91cee3f976f96da3e55fd
Lenovo <xxx@xxx.com>
2020-04-14 17:37:05 +0800

    测试

---
+++ application/admin/controller/LoginController.php
@@ -0,0 +1,10 @@
+<?php
+namespace app\admin\controller;
+
+class LoginController
+{
+    public function index()
+    {
+        return '<h1>3333</h1>';
+    }
+}
--- application/admin/model/User.php
+++ application/admin/model/User.php
@@ -8,20 +8,8 @@
 
 namespace app\admin\model;
 
-
-use Think\Db;
-use think\Exception;
-use think\Log;
 use think\Model;
 class User extends Model
 {
-
-    /**
-     * 查询身份认证车辆未认证的司机所有司机
-     */
-    public function driverList(){
-        $result = Db::name('user')->field('id,username')->select();
-        return $result;
-    }
 
 }
